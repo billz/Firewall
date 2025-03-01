10b171fb0316d1038c716dc8746e0ebcf970f6c1
billz <billzimmerman@gmail.com>
2025-03-01 14:09:01 -0800

    Populate main + basic tabs from firewall template

---
+++ templates/main.php
@@ -0,0 +1,85 @@
+<?php ob_start() ?>
+  <?php if (!RASPI_MONITOR_ENABLED) : ?>
+    <input type="hidden" name="firewall-action" id="firewall-action" value="" />
+    <?php if ($fw_conf["firewall-enable"]) : ?>
+        <input type="submit" class="btn btn-outline btn-primary" value="<?php echo _("Apply changes"); ?>" name="apply-firewall" />
+        <button type="button" class="btn btn-warning firewall-apply" data-firewall-action="firewall-disable" data-bs-toggle="modal" data-bs-target="#firewallModal"/>
+          <?php echo _("Disable Firewall") ?>
+        </button>
+    <?php else : ?>
+        <input type="submit" class="btn btn-outline btn-primary" value="<?php echo _("Save settings"); ?>" name="save-firewall" />
+        <button type="button" class="btn btn-success firewall-apply" data-firewall-action="firewall-enable" data-bs-toggle="modal" data-bs-target="#firewallModal"/>
+          <?php echo _("Enable Firewall") ?>
+        </button>
+    <?php endif ?>
+  <?php endif; ?>
+<?php $buttons = ob_get_clean(); ob_end_clean() ?>
+
+<div class="row">
+  <div class="col-lg-12">
+    <div class="card">
+      <div class="card-header">
+        <div class="row">
+          <div class="col">
+            <i class="fas fa-fire-alt me-2"></i><?php echo _("Firewall"); ?>
+          </div>
+          <div class="col">
+            <button class="btn btn-light btn-icon-split btn-sm service-status float-end">
+              <span class="icon text-gray-600"><i class="fas fa-circle service-status-<?php echo $__template_data['serviceStatus'] ?>"></i></span>
+              <span class="text service-status"><?php echo $__template_data['serviceName'];?> <?php echo $__template_data['serviceStatus'] ?></span>
+            </button>
+          </div>
+        </div><!-- /.row -->
+      </div><!-- /.card-header -->
+
+      <div class="card-body">
+        <?php $status->showMessages(); ?>
+        <form id="frm-firewall" action="<?php echo $__template_data['action']; ?>" method="POST" >
+          <?php echo CSRFTokenFieldTag(); ?>
+
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active" id="firewallbasictab" href="#firewallbasic" aria-controls="basic" data-bs-toggle="tab"><?php echo _("Basic"); ?></a></li>
+           <li class="nav-item"><a class="nav-link" id="firewallabouttab" href="#firewallabout" data-bs-toggle="tab"><?php echo _("About"); ?></a></li>
+          </ul>
+
+           <!-- Tab panes -->
+            <div class="tab-content">
+              <?php echo renderTemplate("tabs/basic", $__template_data, $__template_data['pluginName']) ?>
+             <?php echo renderTemplate("tabs/about", $__template_data, $__template_data['pluginName']) ?>
+            </div><!-- /.tab-content -->
+
+          <?php echo $buttons ?>
+
+          </form>
+      </div><!-- /.card-body -->
+      <div class="card-footer"> <?php echo _("Information provided by iptables"); ?></div>
+    </div><!-- /.card -->
+  </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+
+<!-- Custom Plugin JS -->
+<script src="/app/js/plugins/DynDNS.js"></script>
+
+<!-- Modal -->
+<div class="modal fade" id="firewallModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="modal-title" id="ModalLabel">
+          <i class="fas fa-fire-alt me-2"></i><?php if($fw_conf["firewall-enable"]) echo _("Disable Firewall"); else echo _("Enable Firewall"); ?>
+        </div>
+      </div>
+      <div class="modal-body">
+        <div class="col-md-12 mb-3 mt-1">
+          <?php echo _("Changing the firewall status may disrupt or allow incoming traffic. Choose <strong>Proceed</strong> to continue."); ?>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal"><?php echo _("Cancel"); ?></button>
+        <button type="button" id="js-firewall" data-action="execute" class="btn btn-outline btn-primary"><?php echo _("Proceed"); ?></button>
+      </div>
+    </div>
+  </div>
+</div>
+
---
+++ templates/tabs/basic.php
@@ -0,0 +1,68 @@
+<!-- basic settings tab -->
+<div class="tab-pane active" id="firewallbasic">
+  <div class="row">
+    <div class="col-lg-12">
+      <h4><?php echo _("Client Firewall"); ?></h4>
+      <?php if ( $fw_conf["firewall-enable"]) : ?>
+         <i class="fas fa-circle me-2 service-status-up"></i><?php echo _("Firewall is ENABLED"); ?>
+      <?php else : ?>
+         <i class="fas fa-circle me-2 service-status-down"></i><?php echo _("Firewall is OFF"); ?>
+      <?php endif ?>
+      <div class="row">
+        <div class="col-md-6">
+          <p class="me-2">
+            <small>
+              <?php echo _("The default firewall will only allow outgoing and already established traffic."); ?><br />
+              <?php echo _("No incoming UDP traffic is allowed."); ?><br />
+              <?php printf(_("There are no restrictions for the access point <code>%s</code>."), $ap_device); ?>
+            </small>
+          </p>
+        </div>
+      </div>
+        <h5><?php echo _("Exception: Service"); ?></h4>
+        <div class="row">
+          <div class="mb-3 col-md-6">
+              <div class="form-check form-switch">
+                  <input class="form-check-input" id="ssh-enable" type="checkbox" name="ssh-enable" value="1" aria-describedby="exception-description" <?php if ($fw_conf["ssh-enable"]) echo "checked"; ?> >
+                  <label class="form-check-label" for="ssh-enable"><?php echo _("allow SSH access on port 22") ?></label>
+              </div>
+              <div class="form-check form-switch">
+                  <input class="form-check-input" id="http-enable" type="checkbox" name="http-enable" value="1" aria-describedby="exceptions-description" <?php if ($fw_conf["http-enable"]) echo "checked"; ?> >
+                  <label class="form-check-label" for="http-enable"><?php echo _("allow access to the RaspAP GUI on port 80 or 443") ?></label>
+              </div>
+              <p class="mb-0" id="exceptions-description">
+                  <small><?php echo _("Allow incoming connections for some services from the internet side.") ?></small>
+              </p>
+          </div>
+        </div>
+        <h5><?php echo _("Exception: network device"); ?></h4>
+        <div class="row">
+          <div class="mb-3 col-md-6">
+              <label for="excl-device"><?php echo _("Exclude device(s)") ?></label>
+              <input class="form-control" id="excl-devices" type="text" name="excl-devices" value="<?php echo $fw_conf["excl-devices"] ?>" aria-describedby="exclusion-description"  >
+              <p class="mb-0" id="exclusion-description">
+                <small>
+                  <?php echo _("Exclude the given network device(s) (separated by a blank or comma) from firewall rules."); ?><br />
+                  <?php printf(_("Current client devices: <code>%s</code>"), $str_clients); ?><br />
+                  <?php printf(_("The access point <code>%s</code> is per default excluded."), $ap_device); ?>
+                </small>
+              </p>
+          </div>
+        </div>
+        <h5><?php echo _("Exception: IP-Address"); ?></h4>
+        <div class="row">
+          <div class="mb-3 col-md-6">
+              <label for="excluded-ips"><?php echo _("Allow incoming connections from") ?></label>
+              <input class="form-control" id="excluded-ips" type="text" name="excluded-ips" value="<?php echo $fw_conf["excluded-ips"] ?>" aria-describedby="excl-ips-description"  >
+              <p class="mb-0" id="excl-ips-description">
+                <small>
+                  <?php echo _("For the given IP-addresses (separated by a blank or comma) the incoming connection (via TCP and UDP) is accepted."); ?><br />
+                  <?php echo _("This is required for an OpenVPN via UDP or Wireguard connection."); ?><br />
+                  <?php if ( !empty($vpn_ips) ) printf (_("The list of configured VPN server IP addresses: <code><b>%s</b></code>"), $vpn_ips); ?>
+                </small>
+              </p>
+          </div>
+        </div>
+    </div><!-- /.row -->
+  </div><!-- /.tab-pane | basic tab -->
+
